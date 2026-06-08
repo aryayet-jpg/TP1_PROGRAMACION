749838bcc3a0246c1335253ed131c585565bfd17
Malena <mal.aguirre33@gmail.com>
2026-06-08 02:34:20 -0300

    Corregir codigo de reglas

--- reglas.py
+++ reglas.py
@@ -1,4 +1,4 @@
-# 1. Validación CPU y RAM
+# 1. Verificar CPU y RAM
 
 def regla_cpu_ram_error (cpu: float, ram: float) -> bool:
     if cpu > 80 and ram > 80:
@@ -7,7 +7,7 @@ def regla_cpu_ram_error (cpu: float, ram: float) -> bool:
         return False 
     
 
-# 2. Validación Disco y Procesos
+# 2. Verificar Disco y Procesos
 
 def regla_disco_procesos_error(disco: float, procesos: int) -> bool:
 
@@ -17,7 +17,7 @@ def regla_disco_procesos_error(disco: float, procesos: int) -> bool:
         return False
 
 
-# 3. Validación Firewall
+# 3. Verificar Firewall
 
 def regla_firewall_error(firewall: str) -> bool:
     if not firewall == "Activo":
@@ -26,7 +26,7 @@ def regla_firewall_error(firewall: str) -> bool:
         return False
         
 
-# 4: Alerta de estado óptimo o normal
+# 4: Verificar alerta de estado óptimo o normal
 
 def regla_estado_optimo(cpu: float, ram: float) -> bool:
     if (cpu >= 40 and cpu <= 70) and (ram >= 40 and ram <= 70): 
@@ -35,7 +35,7 @@ def regla_estado_optimo(cpu: float, ram: float) -> bool:
          return False
 
 
-# 5. Validación alerta servidor web
+# 5. Verificar alerta servidor web
 def regla_servidor_web(servidor: int, usuarios: int, cpu: float) -> bool:   
     if servidor == 1 and usuarios > 100 and cpu > 85:
         return True
@@ -43,7 +43,7 @@ def regla_servidor_web(servidor: int, usuarios: int, cpu: float) -> bool:
         return False    
     
 
-# 6. Validación alerta base de datos
+# 6. Verificar alerta base de datos
     
 def regla_base_datos_disco_ram(servidor: int, disco: float, ram: float) -> bool:
     if servidor == 2 and disco < 20 and ram > 70:
